2108cf9ebd2cc8693ba472798bacdbfd2f792fdd
Naveen Kumar Meena <2022pcecrnaveen038@poornima.org>
2025-03-26 13:05:47 +0530

    Create Dutch National Flag Algorithm.py

---
+++ Dutch National Flag Algorithm.py
@@ -0,0 +1,21 @@
+def sort_array(arr):
+    low, mid, high = 0, 0, len(arr) - 1
+
+    while mid <= high:
+        if arr[mid] == 0:
+            arr[low], arr[mid] = arr[mid], arr[low]
+            low += 1
+            mid += 1
+        elif arr[mid] == 1:
+            mid += 1
+        else:  # arr[mid] == 2
+            arr[mid], arr[high] = arr[high], arr[mid]
+            high -= 1
+
+    return arr
+
+
+N = int(input().strip())
+arr = list(map(int, input().strip().split()))
+sorted_arr = sort_array(arr)
+print(*sorted_arr)
