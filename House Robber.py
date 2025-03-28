8cb0da5db5ce394acb5cea50657d7658877430a8
Naveen Kumar Meena <2022pcecrnaveen038@poornima.org>
2025-03-28 10:33:38 +0530

    Update House Robber.py

--- House Robber.py
+++ House Robber.py
@@ -1,21 +1,16 @@
-def max_robbery(n, nums):
-    if n == 0:
-        return 0
-    if n == 1:
-        return nums[0]
+def Robbery(n, cash):
+    if n == 0: return 0
+    if n == 1: return cash[0]
     
     dp = [0] * n
-    dp[0] = nums[0]
-    dp[1] = max(nums[0], nums[1])
+    dp[0] = cash[0]
+    dp[1] = max(cash[0], cash[1])
     
     for i in range(2, n):
-        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        dp[i] = max(dp[i-1], dp[i-2] + cash[i])
     
     return dp[n-1]
 
-# Input reading
 n = int(input())
-nums = list(map(int, input().split()))
-
-# Output the result
-print(max_robbery(n, nums))
+cash = list(map(int, input().split()))
+print(Robbery(n, cash))
